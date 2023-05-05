2960e1206c714128825bda67c65337eceec7e71b
Kevin Wilson <kevin@partcoffee.com>
2023-05-05 14:07:12 +0100

    feat: add CategorySelector

---
+++ app/src/components/CategorySelector/CategorySelector.tsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useMemo, useState } from "react";
+import { useCategories } from "../../hooks/useCategories";
+import { CategoryButton } from "../CategoryButton";
+import styles from "./CategorySelector.module.css";
+
+interface CategorySelectorProps {
+  onSelect: (category: string) => void;
+}
+
+export function CategorySelector({ onSelect }: CategorySelectorProps) {
+  const [filter, setFilter] = useState("");
+  const { categories, isLoading, error, refetch } = useCategories();
+  const filteredCategories = useMemo(
+    () =>
+      categories.filter((category) =>
+        category.name.toLowerCase().includes(filter.toLowerCase())
+      ),
+    [categories, filter]
+  );
+
+  if (isLoading) {
+    return (
+      <p role="alert" aria-live="assertive">
+        Let's find the animals!
+      </p>
+    );
+  }
+
+  if (!categories || error) {
+    return (
+      <div>
+        <p>It looks like the animals are still sleeping!</p>
+
+        <button onClick={refetch}>Wake Them Up!</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.filter}>
+        <label htmlFor="filter">Type an animal name:</label>
+        <input
+          className={styles.filterInput}
+          id="filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+
+      {!Boolean(filteredCategories.length) && (
+        <p>
+          Oh no! We can't find any of those animals just now. Why not try
+          searching for something else?
+        </p>
+      )}
+
+      {Boolean(filteredCategories.length) && (
+        <ul className={styles.categories}>
+          {filteredCategories.map(({ name }) => (
+            <li key={name}>
+              <CategoryButton name={name} onClick={() => onSelect(name)} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
