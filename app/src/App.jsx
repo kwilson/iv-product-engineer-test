987fd3063dfb3cec85a1ebc9926a038685fb462f
Kevin Wilson <kevin@partcoffee.com>
2023-05-07 18:41:52 +0100

    feat: add gif display to app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,23 @@
 import { createClient, Provider } from 'urql';
 import { CategorySelector } from './components/CategorySelector';
 
+import styles from './App.module.css'
+import { useState } from 'react';
+import { GifDisplay } from './components/GifDisplay';
+
 let client = createClient({
   url: 'http://localhost:8080/v1/graphql',
 });
 
 function App() {
+  const [selectedValue, setSelectedValue] = useState('');
+
   return (
     <Provider value={client}>
-      <CategorySelector />
+      <div className={styles.container}>
+        <CategorySelector onSelect={setSelectedValue} selectedValue={selectedValue} />
+        <GifDisplay selectedValue={selectedValue} />
+      </div>
     </Provider>
   )
 }
